4ee294b1a29fcd267f769fe0666b17ff1b266f56
gracekyalo <gracendanu27@gmail.com>
2022-11-07 15:35:03 +0300

    Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
 
-  
+    def favorite_product
+        self.products.order("reviews.star_rating DESC").first
+    end
 end
