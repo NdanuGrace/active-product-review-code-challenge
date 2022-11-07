5d07e5051cf4f283ab14d1111dfafeb1e1c730f1
gracekyalo <gracendanu27@gmail.com>
2022-11-07 05:41:39 +0300

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # This will delete any existing rows from the Product and User tables
 # so you can run the seed file multiple times without having duplicate entries in your database
+require "faker"
+
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
@@ -22,5 +24,13 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
+review1 = Review.create(star_rating: 1, comment: "The product is of poor quality", user_id: 1, product_id:1)
+review2 = Review.create(star_rating: 2, comment: "The product not upto standard", user_id: 3, product_id:3)
+review3 = Review.create(star_rating: 3, comment: "The product is average", user_id:2, product_id: 2)
+review4 = Review.create(star_rating: 4, comment: "The product is somehow good quality", user_id: 3, product_id: 2)
+review5 = Review.create(star_rating: 5, comment: "The product is of good quality", user_id: 3, product_id: 4)
+review6 = Review.create(star_rating: 5, comment: "The product is of good quality", user_id:2, product_id: 5)
+
+
 
 puts "Seeding done!"
